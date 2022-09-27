962d3dd1710d4e4a8029381d6a8691734f3f0afa
keylingobando <keyling.obandofonseca@tiernoparla.es>
2022-09-27 18:58:07 +0200

    añadp src main

---
+++ src/main/java/com/mycompany/ejecicioselementales/holaMundo.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.ejecicioselementales;
+
+/**
+ *
+ * @author alumno
+ */
+public class holaMundo {
+    public static void main(String[] args) {
+      System.out.println("hola munda");
+        
+        
+    }
+}
